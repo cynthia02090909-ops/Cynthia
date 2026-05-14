b53bf8c1111bfa7fcfb84c856d37d33e2c5210a7
汪洁 <cynthia02090909@gmail.com>
2026-05-15 00:06:30 +0800

    Update server.js

--- task-app/server.js
+++ task-app/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
-const cors = require('cors');
-app.use(cors());
 
 const app = express();
 const PORT = process.env.PORT || 3000;
